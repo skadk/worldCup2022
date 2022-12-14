0df813da3a4c93cd6684b09d1430b9b3117f936a
SHJWON <sjw3344@gmail.com>
2022-12-14 20:07:25 +0900

    myPage시작

---
+++ world_cup_2022_projectW/src/main/java/com/world_cup_2022/projectW/service/IMyPageService.java
@@ -0,0 +1,18 @@
+package com.world_cup_2022.projectW.service;
+
+import com.world_cup_2022.projectW.model.MemberVO;
+
+
+public interface IMyPageService {
+
+
+	
+
+	/* public MemberVO detailViewMyPage(String memId); */// 상세 상품 조회
+	
+    public MemberVO detailViewMyPage(String memId);
+	
+
+
+	
+}
